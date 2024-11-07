b5ea8e83a0125a7c09d03331007323ba730aa503
Giovanni Ricca <giovanniricca@proton.me>
2024-11-07 09:32:18 +0100

    stone: Fix libgoodixhwfingerprint ELF checks
    Change-Id: Ic6d6d247a13fc7cdfc0eceffde6a98dde8759b19

--- extract-files.py
+++ extract-files.py
@@ -58,6 +58,8 @@ blob_fixups: blob_fixups_user_type = {
         .clear_symbol_version('AHardwareBuffer_lock')
         .clear_symbol_version('AHardwareBuffer_release')
         .clear_symbol_version('AHardwareBuffer_unlock'),
+    'vendor/lib64/libgoodixhwfingerprint.so': blob_fixup()
+        .replace_needed('libvendor.goodix.hardware.biometrics.fingerprint@2.1.so', 'vendor.goodix.hardware.biometrics.fingerprint@2.1.so'),
     'vendor/lib64/libvendor.goodix.hardware.biometrics.fingerprint@2.1.so': blob_fixup()
         .replace_needed('libhidltransport.so', 'libhidlbase_shim.so'),
     ('vendor/lib64/libwvhidl.so','vendor/lib64/mediadrm/libwvdrmengine.so'): blob_fixup()
