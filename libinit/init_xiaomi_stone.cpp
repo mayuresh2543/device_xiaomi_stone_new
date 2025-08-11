2ce0790d2bf35135a2f14f89a50197251ff225da
mayuresh2543 <mayureshnanal846@gmail.com>
2025-08-11 20:04:50 +0530

    stone: Update blobs from OS2.0.4.0.UMQEUXM
    Update fingerprints of other regions while at it

--- libinit/init_xiaomi_stone.cpp
+++ libinit/init_xiaomi_stone.cpp
@@ -18,7 +18,7 @@ void vendor_load_properties() {
             "moonstone",
             "POCO X5 5G",
             "_blair",
-            "POCO/moonstone_p_global/moonstone:14/UKQ1.231003.002/V816.0.17.0.UMPMIXM:user/release-keys"
+            "POCO/moonstone_p_global/moonstone:14/UKQ1.231003.002/V816.0.19.0.UMPMIXM:user/release-keys"
         },
         { //MOONSTONE_IN
             "moonstone_p_in",
@@ -26,7 +26,7 @@ void vendor_load_properties() {
             "moonstone",
             "POCO X5 5G",
             "_blair",
-            "POCO/moonstone_p_in/moonstone:14/UKQ1.231003.002/V816.0.15.0.UMPINXM:user/release-keys"
+            "POCO/moonstone_p_in/moonstone:14/UKQ1.231003.002/V816.0.17.0.UMPINXM:user/release-keys"
         },
         { //SUNSTONE_CN
             "sunstone_cn",
@@ -34,7 +34,7 @@ void vendor_load_properties() {
             "sunstone",
             "Redmi Note 12 5G",
             "_blair_lite",
-            "Redmi/sunstone/sunstone:14/UKQ1.230917.001/OS2.0.4.0.UMQCNXM:user/release-keys"
+            "Redmi/sunstone/sunstone:14/UKQ1.230917.001/OS2.0.5.0.UMQCNXM:user/release-keys"
         },
         { //SUNSTONE_GLOBAL
             "sunstone_global",
@@ -42,7 +42,7 @@ void vendor_load_properties() {
             "sunstone",
             "Redmi Note 12 5G",
             "_blair_lite",
-            "Redmi/sunstone_global/sunstone:14/UKQ1.240624.001/OS2.0.1.0.UMQMIXM:user/release-keys"
+            "Redmi/sunstone_global/sunstone:14/UKQ1.240624.001/OS2.0.3.0.UMQMIXM:user/release-keys"
         },
         { //SUNSTONE_IN
             "sunstone_in",
@@ -58,7 +58,7 @@ void vendor_load_properties() {
             "sunstone",
             "Redmi Note 12R Pro",
             "_blair_lite",
-            "Redmi/sunstone/sunstone:14/UKQ1.230917.001/OS2.0.4.0.UMQCNXM:user/release-keys"
+            "Redmi/sunstone/sunstone:14/UKQ1.230917.001/OS2.0.5.0.UMQCNXM:user/release-keys"
         }
     };
     
