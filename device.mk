7c417a1827ab1d497498b55a15d9facec1265813
mayuresh <mayureshnanal846@gmail.com>
2026-03-18 10:38:01 +0530

    Revert "stone: switch to config_set_bool"
    This reverts commit 60ed1a9030df7e62aa10ab8ade79bcda5c7497e5.

--- device.mk
+++ device.mk
@@ -97,7 +97,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libutilscallstack.vendor
 
-$(call soong_config_set_bool,camera,override_format_from_reserved,true)
+$(call soong_config_set,camera,override_format_from_reserved,true)
 
 # Datura
 PRODUCT_PACKAGES += Datura
@@ -134,7 +134,7 @@ PRODUCT_COPY_FILES += \
     hardware/qcom-caf/sm8350/display/config/snapdragon_color_libs_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/snapdragon_color_libs_config.xml \
     $(LOCAL_PATH)/configs/display_id_4630947218746568833.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630947218746568833.xml
 
-$(call soong_config_set_bool,stagefright,target_disable_thumbnail_block_model,true)
+$(call soong_config_set,stagefright,target_disable_thumbnail_block_model,true)
 
 # DRM
 PRODUCT_PACKAGES += \
