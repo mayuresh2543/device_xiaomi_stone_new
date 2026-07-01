ae4e1a65058856cb899ec5375701ec2f382a956d
Nolen Johnson <johnsonnolen@gmail.com>
2026-07-01 01:34:49 -0400

    stone: Use legacy libion implementation
    * At least one blob directly depends on this, with
      many other bits and pieces likely.
    
    Change-Id: Ic063618d6942a4f6e90dd7e21698e36d14113d06

--- device.mk
+++ device.mk
@@ -222,6 +222,9 @@ PRODUCT_PACKAGES += \
     uinput-goodix.idc \
     uinput-goodix.kl
 
+# ION
+$(call soong_config_set_bool,libion,legacy_impl,true)
+
 # IPACM
 PRODUCT_PACKAGES += \
     ipacm \
