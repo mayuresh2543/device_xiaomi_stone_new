ad6f2df65cde9234fc6b85e9967bf812bb9859e2
mayuresh2543 <mayureshnanal846@gmail.com>
2026-08-14 13:09:04 +0530

    stone: Use Clang r596125 for kernel build
    No reason to use older default clang when a newer clang is stable and confirmed to be working fine

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -96,6 +96,7 @@ BOARD_KERNEL_CMDLINE := \
     ip6table_raw.raw_before_defrag=1 \
     firmware_class.path=/vendor/firmware
 
+TARGET_KERNEL_CLANG_VERSION := r596125
 TARGET_KERNEL_CONFIG := stone_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/stone
 TARGET_KERNEL_NO_GCC := true
